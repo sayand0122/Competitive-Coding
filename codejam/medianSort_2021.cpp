ef09d78f3992148fc8933ec39c92290545e974e0
sayand0122 <sayandutta.jan@gmail.com>
2021-03-29 01:41:16 +0530

    strategies

---
+++ codejam/medianSort_2021.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb(x) push_back(x)
+#define MAX(x, y) ((x) > (y)) ? (x) : (y)
+#define MIN(x, y) ((x) < (y)) ? (x) : (y)
+#define mod(x) ((x) >= 0) ? (x) : (-x)
+#define INF LONG_LONG_MAX
+#define godspeed                      \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+
+#define F0R(i, a, b) for (int i = (a); i < (b); i++)
+#define FOR(i, a) for (int i = 0; i < (a); i++)
+#define RFOR(i, a, b) for (int i = (a); i >= b; i--)
+
+int solve()
+{
+}
+
+int32_t main()
+{
+    godspeed;
+    int t;
+    cin >> t;
+    int i = 0;
+    while (t--)
+    {
+        cout << "Case #" << (++i) << ": " << solve() << "\n";
+    }
+    return 0;
+}
