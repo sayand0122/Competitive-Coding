f4050623f762a30cde36b2bd7f6c5056c667d10f
sayand0122 <sayandutta.jan@gmail.com>
2021-04-24 16:15:07 +0530

    div 718 update

---
+++ Codeforces/Round 718/A.cpp
@@ -0,0 +1,60 @@
+// https://codeforces.com/contest/1517/problem/A
+// Sayan Dutta
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define mp make_pair
+#define lld long double
+
+#define ms(arr, v) memset(arr, v, sizeof(arr))
+#define MAX(x, y) ((x) > (y)) ? (x) : (y)
+#define MIN(x, y) ((x) < (y)) ? (x) : (y)
+#define mod(x) ((x) >= 0) ? (x) : (-x)
+#define all(v) v.begin(), v.end()
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define RFOR(i, a, b) for (int i = (a); i > (b); i--)
+#define sz(x) (int)(x).size()
+
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+
+const int INF = 1000000009;
+const lld pi = 3.14159265358979323846;
+const int MX = 0; // check the limits, dummy
+
+#define godspeed                                                                                                       \
+    ios_base::sync_with_stdio(false);                                                                                  \
+    cin.tie(NULL)
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        if (n % 2050 == 0)
+        {
+            ll x = n / 2050;
+            ll sum = 0;
+            while (x > 0)
+            {
+                sum += x % 10;
+                x /= 10;
+            }
+            cout << sum << "\n";
+        }
+        else
+        {
+            cout << "-1\n";
+        }
+    }
+}
